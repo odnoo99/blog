219ef905f64feaaabfd174783dd5ea6cf4f60e06
odonchimeg <z.odnoo@gmail.com>
2021-04-03 14:49:43 +0700

    bootstrap nemsen

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-
+import Alert from "react-bootstrap/Alert";
 export default function Home() {
-  return <div className={styles.container}>Ховд их сургууль</div>;
+  return <div className={styles.container}>
+   {[
+  'primary',
+  'secondary',
+  'success',
+  'danger',
+  'warning',
+  'info',
+  'light',
+  'dark',
+].map((variant, idx) => (
+  <Alert key={idx} variant={variant}>
+    This is a {variant} alert—check it out!
+  </Alert>
+))}</div>;
 }
